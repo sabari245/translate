c3b5afe2b2304db1c58a19a76cb7fe6665e7d46b
sabari245 <sabari.h.dev@gmail.com>
2023-07-26 21:49:19 +0530

    finished v1

--- main.py
+++ main.py
@@ -1,35 +1,36 @@
 import re
 import os
-from deep_translator import GoogleTranslator
+
 
 def read_file(path: str) -> str:
-    
     res = ""
     with open(path, "r", encoding="utf8") as fp:
         res = res.join(fp.readlines())
 
     return res
 
+
 def find_chinese(string: str) -> list[str]:
-    return set(re.findall(r'[\u4e00-\u9fff]+', string))
+    return set(re.findall(r"[\u4e00-\u9fff]+", string))
 
 
-def translate(chinese_words):
-    res = []
-    for word in chinese_words:
-        res.append(GoogleTranslator(source='zh-CN', target='en').translate(word))
-    return res
+# def translate(chinese_words):
+#     res = []
+#     for word in chinese_words:
+#         res.append(GoogleTranslator(source='zh-CN', target='en').translate(word))
+# return res
+
 
 def get_files() -> list[str]:
     directory_path = "./src"
-    extension = ".tsx"
+    extensions = [".tsx", ".ts", ".jsx", ".js", ".md", ".mdx", ".sh"]
     result = []
     for dirpath, _, filenames in os.walk(directory_path):
         for filename in filenames:
-            if filename.endswith(extension):
-                result.append(os.path.join(dirpath, filename))
+            for extension in extensions:
+                if filename.endswith(extension):
+                    result.append(os.path.join(dirpath, filename))
     return result
-
 
 
 if __name__ == "__main__":
@@ -41,8 +42,16 @@ if __name__ == "__main__":
         chinese = find_chinese(read_file(each))
         chinese_words.update(chinese)
 
-    print(list(chinese_words))
+    # print(list(chinese_words))
+
+    chinese_words = list(chinese_words)
+
+    with open("res.txt", "w", encoding="utf8") as f:
+        i = 0
+        for each in chinese_words:
+            f.write(each + "\n")
+            i += 1
+            print(f"\rcompleted:{100 - int(len(chinese_words) / i)}", end="")
 
     # print(chinese)
     # print(translate(chinese))
-
